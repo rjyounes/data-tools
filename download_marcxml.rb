837b285ec039cbbd6090c7958225e39c11d07c6a
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-07-07 14:22:49 -0400

    Correct range

--- download_marcxml.rb
+++ download_marcxml.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/ruby -w
 
-# (1..179).each do |n|
-(101..179).each do |n|
+# TODO Rewrite to take two args
+# Pad based on number of digits of second arg
+
+(1..179).each do |n|
+# (101..179).each do |n|
   s = n.to_s.rjust(3, '0')
   filename = "bib.#{s}.xml"
   `curl http://da-data.library.cornell.edu/bibdata/#{filename} > #{filename}`  
