9f28fb196b4cd690bb034788bd9d3d8ac70b5e9a
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-07-13 15:11:11 -0400

    Add output statements

--- count_records_in_marcxml_files.rb
+++ count_records_in_marcxml_files.rb
@@ -3,10 +3,11 @@
 # Count the number of records in a directory of marcxml files. 
 
 search_string = "<record"
-count = 0
+total = 0
 Dir.glob("*.xml") do |f|
-  # puts f
   text = IO.read f
-  count += text.scan(search_string).length
+  count = text.scan(search_string).length
+  puts "Number of records in file #{f}: #{count}"
+  total += count
 end
-puts count
+puts "Total number of records in all files: " + total
