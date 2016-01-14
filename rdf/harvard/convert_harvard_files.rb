cb8ccd322d99c9a832a4460990b38861dc2b4efc
Rebecca Younes <rjy7@lib-rjy7.local>
2016-01-13 20:30:29 -0500

    Harvard conversion script

--- rdf/harvard/convert_harvard_files.rb
+++ rdf/harvard/convert_harvard_files.rb
@@ -12,13 +12,11 @@ require 'fileutils'
 jar=ARGV[0]
 Dir.mkdir("rdfxml") unless File.exists?("rdfxml")
 Dir.mkdir("ntriples") unless File.exists?("ntriples")
-Dir.mkdir("originals") unless File.exists?("originals")
 
 Dir.glob("ab.*").each do |f|
 
   basename = f.sub("ab.bib", "harvard").sub(/2015.*_(\d+)/, "\\1")
   rdffile = basename + ".rdf"
-  FileUtils.cp(f, "originals")
   File.rename(f, rdffile)
   ntfile = basename + ".nt"
   cmd = "java -jar #{jar} #{rdffile} #{ntfile}"
